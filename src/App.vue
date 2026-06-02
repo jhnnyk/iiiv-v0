cd715162a9b554647ebfb6570d78859998c26149
John Knotts <jhnnyk@gmail.com>
2026-06-02 16:57:12 -0600

    very basic styles

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>Indie Index</h1>
 </template>
 
-<style scoped></style>
+<style>
+body {
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  text-align: center;
+  padding: 200px;
+  background-color: rgb(11, 11, 11);
+  color: rgb(203, 210, 216);
+}
+</style>
