6edace6f038e3f6f735f16426bb0d9e49d470f86
John Knotts <jhnnyk@gmail.com>
2026-06-06 15:18:09 -0600

    home page placeholder

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomePage from '@/pages/HomePage.vue'
 import LoginPage from '@/pages/LoginPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomePage,
+    },
     {
       path: '/login',
       name: 'login',
