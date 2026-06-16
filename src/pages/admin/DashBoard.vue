cdc96bb491b35f960297e7acba84ea88eca1efac
John Knotts <jhnnyk@gmail.com>
2026-06-16 13:50:26 -0600

    add dashboard placeholder

---
+++ src/pages/admin/DashBoard.vue
@@ -0,0 +1,3 @@
+<template>
+  <p>dashboard here</p>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '@/pages/HomePage.vue'
 import LoginPage from '@/pages/LoginPage.vue'
 import AddVideo from '@/pages/admin/AddVideo.vue'
+import DashBoard from '@/pages/admin/DashBoard.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,11 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: LoginPage,
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: DashBoard,
     },
     {
       path: '/video/create',
