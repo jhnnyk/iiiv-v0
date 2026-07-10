2fd0565bb46e06bf9d06a3d862707452ca59048e
John Knotts <jhnnyk@gmail.com>
2026-07-10 17:05:10 -0600

    show embeded video on home screen

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,3 +1,18 @@
+<script setup>
+import { onMounted } from 'vue'
+import { useBlockbuster } from '@/stores/blockbuster'
+
+const blockbuster = useBlockbuster()
+
+onMounted(() => blockbuster.fetchVideos())
+</script>
+
 <template>
-  <p>home content here</p>
+  <p v-if="blockbuster.loading">.... loading ....</p>
+  <p v-else-if="!blockbuster.videos.length">No posts yet.</p>
+
+  <article v-for="video in blockbuster.videos" :key="video.id">
+    <div v-html="video.embedCode"></div>
+    <h2>{{ video.title }}</h2>
+  </article>
 </template>
